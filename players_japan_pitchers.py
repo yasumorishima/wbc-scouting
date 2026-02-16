d456dbaf2be5c97e881d1fc4a626ff18d42e47a4
yasumorishima <fw_yasu11@hotmail.com>
2026-02-17 07:06:39 +0900

    fix: Sugano team FA -> Rockies

--- players_japan_pitchers.py
+++ players_japan_pitchers.py
@@ -4,7 +4,7 @@ JAPAN_PITCHERS = [
     {"name": "Yoshinobu Yamamoto", "mlbam_id": 808967, "team": "Dodgers", "throws": "R", "role": "SP"},
     {"name": "Yusei Kikuchi",      "mlbam_id": 579328, "team": "Angels",  "throws": "L", "role": "SP"},
     {"name": "Yuki Matsui",        "mlbam_id": 673513, "team": "Padres",  "throws": "L", "role": "RP"},
-    {"name": "Tomoyuki Sugano",    "mlbam_id": 608372, "team": "FA",      "throws": "R", "role": "SP"},  # Former Orioles
+    {"name": "Tomoyuki Sugano",    "mlbam_id": 608372, "team": "Rockies", "throws": "R", "role": "SP"},
 ]
 
 PITCHER_BY_ID   = {p['mlbam_id']: p for p in JAPAN_PITCHERS}
